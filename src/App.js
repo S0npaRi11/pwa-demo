7e8201efe7f9de0214ac8caf9341e08ace7956b1
s0npaRi11 <paragmahale36@gmail.com>
2021-07-15 19:06:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import React ,{ useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const [count, setCount] = useState(1)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello!! </h1>
+
+      <p> This is a progressive web app demo with react!! </p>
+
+      <p> Press the button to count. </p>
+
+      <div className="center">
+        <h2> { count } </h2>
+
+        <button onClick={() => setCount(count+1)}> Press Me </button>
+      </div>
+
     </div>
   );
 }
